cb0330092ce257665e1a585679f78a134cac88e2
Cristian Urbainski <cristianurbainskips@gmail.com>
2020-08-26 00:11:58 -0300

    Adicionado filtro cors

--- backend/src/main/java/br/com/urbainski/backend/BackendApplication.java
+++ backend/src/main/java/br/com/urbainski/backend/BackendApplication.java
@@ -3,14 +3,34 @@ package br.com.urbainski.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 public class BackendApplication {
 
+    private static final String ORIGIN_ALLOWED = "http://localhost:4200";
+
     public static void main(String[] args) {
 
         SpringApplication.run(BackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Authorization", "Content-Type", "Accept")
+                        .allowCredentials(true)
+                        .maxAge(3600)
+                        .allowedOrigins(ORIGIN_ALLOWED);
+            }
+        };
     }
 
 }
